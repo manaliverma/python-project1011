d7fab775ffb26246a2ef3d6d4f37993e18f04fb0
Manali Verma <gulliblemanaliverma@gmail.com>
2020-10-01 01:20:38 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -5,7 +5,7 @@ while (True):
  userinput = input("enter the item price or press q to quit \n")
  if (userinput != 'q'):
      sum = sum + int(userinput)
-     print(f"order total so far :{sum} \n")
+     print(f"order total :{sum} \n")
 
  else:
     print(f"{sum} thanks for shopping")
